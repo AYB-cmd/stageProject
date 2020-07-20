97ff771127ffb043958eab4b24018f761aaa0a1d
AYB-cmd <aeljelladi@gmail.com>
2020-07-20 15:33:50 +0100

    inc

--- pages/apropos.php
+++ pages/apropos.php
@@ -1,67 +1,9 @@
 <?php include "../lang/fr.php"; ?>
 <?php $section = "apropos"; ?>
-<!DOCTYPE html>
-<html>
 
-<head>
+<?php include "../includes/header.php" ?>
 
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <title>Model</title>
-
-    <meta name="keywords" content="HTML5 Template" />
-    <meta name="description" content="Porto - Responsive HTML5 Template">
-    <meta name="author" content="okler.net">
-
-
-    <link rel="shortcut icon" href="../img/favicon.ico" type="image/x-icon" />
-    <link rel="apple-touch-icon" href="../img/apple-touch-icon.png">
-
-
-    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, shrink-to-fit=no">
-
-
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700,800%7CShadows+Into+Light%7CPlayfair+Display:400" rel="stylesheet" type="text/css">
-
-
-    <link rel="stylesheet" href="../vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../vendor/fontawesome-free/css/all.min.css">
-    <link rel="stylesheet" href="../vendor/animate/animate.min.css">
-    <link rel="stylesheet" href="../vendor/simple-line-icons/css/simple-line-icons.min.css">
-    <link rel="stylesheet" href="../vendor/owl.carousel/assets/owl.carousel.min.css">
-    <link rel="stylesheet" href="../vendor/owl.carousel/assets/owl.theme.default.min.css">
-    <link rel="stylesheet" href="../css/theme.css">
-    <link rel="stylesheet" href="../css/theme-elements.css">
-    <link rel="stylesheet" href="../vendor/rs-plugin/css/settings.css">
-    <link rel="stylesheet" href="../vendor/rs-plugin/css/layers.css">
-    <link rel="stylesheet" href="../vendor/rs-plugin/css/navigation.css">
-    <link rel="stylesheet" href="../css/skins/skin-corporate-3.css">
-    <link rel="stylesheet" href="../css/custom.css">
-
-
-    <script src="../vendor/modernizr/modernizr.min.js"></script>
-
-</head>
-
-<body class="loading-overlay-showing" data-plugin-page-transition data-loading-overlay data-plugin-options="{'hideDelay': 500}">
-    <div class="loading-overlay">
-        <div class="bounce-loader">
-            <div class="bounce1"></div>
-            <div class="bounce2"></div>
-            <div class="bounce3"></div>
-        </div>
-    </div>
-
-    <div class="body">
-        <?php include "../includes/header.php" ?>
-
-
-
-
-
-
-        <section class="section section-height-3 bg-white m-0 border-0">
+<section class="section section-height-3 bg-white m-0 border-0">
             <div class="container">
                 <div class="row align-items-center justify-content-center">
                     <div class="col-lg-6 pb-sm-4 pb-lg-0 pr-lg-5 mb-sm-5 mb-lg-0">
@@ -110,42 +52,3 @@
         </section>
         <?php include "../includes/promo.php" ?>
         <?php include "../includes/footer.php" ?>
-
-
-        <script src="../vendor/jquery/jquery.min.js"></script>
-        <script src="../vendor/jquery.appear/jquery.appear.min.js"></script>
-        <script src="../vendor/jquery.easing/jquery.easing.min.js"></script>
-        <script src="../vendor/jquery.cookie/jquery.cookie.min.js"></script>
-        <script src="../vendor/popper/umd/popper.min.js"></script>
-        <script src="../vendor/bootstrap/js/bootstrap.min.js"></script>
-        <script src="../vendor/common/common.min.js"></script>
-        <script src="../vendor/jquery.validation/jquery.validate.min.js"></script>
-        <script src="../vendor/jquery.easy-pie-chart/jquery.easypiechart.min.js"></script>
-        <script src="../vendor/jquery.gmap/jquery.gmap.min.js"></script>
-        <script src="../vendor/jquery.lazyload/jquery.lazyload.min.js"></script>
-        <script src="../vendor/isotope/jquery.isotope.min.js"></script>
-        <script src="../vendor/owl.carousel/owl.carousel.min.js"></script>
-        <script src="../vendor/magnific-popup/jquery.magnific-popup.min.js"></script>
-        <script src="../vendor/vide/jquery.vide.min.js"></script>
-        <script src="../vendor/vivus/vivus.min.js"></script>
-
-
-        <script src="../js/theme.js"></script>
-
-
-        <script src="../vendor/rs-plugin/js/jquery.themepunch.tools.min.js"></script>
-        <script src="../vendor/rs-plugin/js/jquery.themepunch.revolution.min.js"></script>
-
-
-        <script src="../js/views/view.contact.js"></script>
-
-
-        <script src="../js/custom.js"></script>
-
-
-        <script src="../js/theme.init.js"></script>
-
-
-</body>
-
-</html>
